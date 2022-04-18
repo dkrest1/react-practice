6aa8492e9485690ecdefd1ebd7056c77bae1dfe5
Akande Oluwatosin <aktosin1@gmail.com>
2022-04-18 19:21:29 +0100

    learning styled componets

---
+++ blog-app/src/store.js
@@ -0,0 +1,6 @@
+import { createStore, action, thunk, computed } from "easy-peasy";
+import api from "./api/api";
+
+export default createStore({
+  // hello
+});
---
+++ styled-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React! Ready to go</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
